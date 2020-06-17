6cee38cc0f42e6b1eb862b80dc68084c791fcdeb
nick-sib <khc1981@mail.ru>
2020-06-18 02:19:08 +0700

    lesson2

---
+++ app/src/main/java/com/jako/android_meteo/SelectCity.kt
@@ -0,0 +1,12 @@
+package com.jako.android_meteo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SelectCity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.city_select)
+    }
+}
